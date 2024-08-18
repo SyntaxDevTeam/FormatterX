92b5af2ebac63694cb64320c765097de4d20427e
SyntaxDevTeam <syntaxerr85@gmail.com>
2024-08-18 09:51:19 +0200

    Initial commit

---
+++ src/main/kotlin/pl/syntaxdevteam/FormatterX.kt
@@ -0,0 +1,15 @@
+package pl.syntaxdevteam
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FormatterX : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+
+        // Plugin shutdown logic
+    }
+}
