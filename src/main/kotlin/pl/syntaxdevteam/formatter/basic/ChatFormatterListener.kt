a59e2f870a2919bdf7fdd6db778e8398da8f8856
SyntaxDevTeam <wieszczy85@gmail.com>
2025-02-06 20:40:13 +0100

    Separated placeholder logic and added group support

--- src/main/kotlin/pl/syntaxdevteam/formatter/basic/ChatFormatterListener.kt
+++ src/main/kotlin/pl/syntaxdevteam/formatter/basic/ChatFormatterListener.kt
@@ -3,6 +3,7 @@ package pl.syntaxdevteam.formatter.basic
 import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
+import net.luckperms.api.cacheddata.CachedMetaData
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -16,21 +17,24 @@ class ChatFormatterListener(
     private val messageHandler: MessageHandler
 ) : Listener {
     private val luckPerms = plugin.luckPerms
+
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     fun onChat(event: AsyncChatEvent) {
         event.isCancelled = true
 
         val player = event.player
         val messageContent = PlainTextComponentSerializer.plainText().serialize(event.message())
-
-        val defaultFormat = plugin.config.getString("chat.defaultFormat")
-            ?: "${messageHandler.getPrefix()} {displayname} » {message}"
-
         val metaData = luckPerms?.getPlayerAdapter(Player::class.java)?.getMetaData(player)
+        val group = metaData?.primaryGroup ?: "default"
+        val format = generateChatFormat(metaData, group, player, messageContent)
+        val finalComponent: Component = messageHandler.formatMixedTextToMiniMessage(format)
 
+        Bukkit.getServer().sendMessage(finalComponent)
+    }
+
+    private fun generateChatFormat(metaData: CachedMetaData?, group: String, player: Player, messageContent: String): String {
         val prefix = metaData?.prefix ?: ""
         val suffix = metaData?.suffix ?: ""
-
         val prefixes = metaData?.prefixes?.entries
             ?.sortedByDescending { it.key }
             ?.joinToString(" ") { it.value } ?: ""
@@ -38,7 +42,6 @@ class ChatFormatterListener(
         val suffixes = metaData?.suffixes?.entries
             ?.sortedByDescending { it.key }
             ?.joinToString(" ") { it.value } ?: ""
-
         val usernameColor = metaData?.getMetaValue("username-color") ?: "<white>"
         val messageColor = metaData?.getMetaValue("message-color") ?: "<gray>"
 
@@ -46,20 +49,17 @@ class ChatFormatterListener(
         val playerName = player.name
         val worldName = player.world.name
 
-        val formattedMessage = defaultFormat
-            .replace("{prefix}", prefix)
-            .replace("{suffix}", suffix)
-            .replace("{prefixes}", prefixes)
-            .replace("{suffixes}", suffixes)
-            .replace("{displayname}", displayName)
-            .replace("{name}", playerName)
-            .replace("{world}", worldName)
-            .replace("{username-color}", usernameColor)
-            .replace("{message-color}", messageColor)
-            .replace("{message}", messageContent)
+        return (plugin.config.getString("chat.group-formats.$group") ?: plugin.config.getString("chat.defaultFormat") ?: "${messageHandler.getPrefix()} {displayname} » {message}")
+             .replace("{prefix}", prefix)
+             .replace("{suffix}", suffix)
+             .replace("{prefixes}", prefixes)
+             .replace("{suffixes}", suffixes)
+             .replace("{displayname}", displayName)
+             .replace("{name}", playerName)
+             .replace("{world}", worldName)
+             .replace("{username-color}", usernameColor)
+             .replace("{message-color}", messageColor)
+             .replace("{message}", messageContent)
 
-        val finalComponent: Component = messageHandler.formatMixedTextToMiniMessage(formattedMessage)
-
-        Bukkit.getServer().sendMessage(finalComponent)
     }
 }
