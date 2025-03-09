e48b8f4f9ff0ebfac6a19a6c273d9a290e982464
SyntaxDevTeam <wieszczy85@gmail.com>
2025-03-09 07:46:55 +0100

    Updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "pl.syntaxdevteam.formatter"
-version = "1.0.0"
+version = "1.1.0"
 description = "The intelligent chat formatting plugin with LuckPerms, PlaceholderAPI, MiniPlaceholder, Vault and VaultUnlocked support!"
 
 repositories {
